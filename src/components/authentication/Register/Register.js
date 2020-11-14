3c2f5908d86e725fcef12ec1c213ffc719e53236
DarkDave <darkdave1185@gmail.com>
2020-11-14 16:14:37 -0700

    users regsterd in backend fix auto switch when logged in

--- src/components/authentication/Register/Register.js
+++ src/components/authentication/Register/Register.js
@@ -1,5 +1,76 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+
+import Axios from "axios";
+import { useHistory } from "react-router-dom";
+import UserContext from "../../../context/UserContext";
+import "./styleRegister.css";
 
 export default function Register() {
-  return <div>Register</div>;
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [passwordCheck, setPasswordCheck] = useState();
+  const [displayName, setDisplayName] = useState();
+  /* const [error, setError] = useState(); */
+
+  const { setUserData } = useContext(UserContext);
+  let history = useHistory();
+
+  const submit = async (e) => {
+    e.preventDefault();
+    /* try { */
+    const newUser = { email, password, passwordCheck, displayName };
+    const registerResponse = await Axios.post(
+      "http://localhost:5000/users/register",
+      newUser
+    );
+    const loginResponse = await Axios.post(
+      "http://localhost:5000/users/login",
+      {
+        email,
+        setPassword,
+      }
+    );
+    console.log(loginResponse.data);
+    setUserData({
+      token: loginResponse.data.token,
+      user: loginResponse.data.user,
+    });
+    localStorage.setItem("auth-token", loginResponse.data.token);
+    history.push("/");
+  }; /* catch (err) {
+      err.response.data.msg && setError(err.response.data.msg);
+    }
+  }; */
+
+  return (
+    <div className="registration">
+      <h3>Register</h3>
+      <form onSubmit={submit} className="form">
+        <label htmlFor="reg-email">Email</label>
+        <input
+          id="reg-email"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="reg-pass">Password</label>
+        <input
+          id="reg-pass"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Verify Password"
+          onChange={(e) => setPasswordCheck(e.target.value)}
+        />
+        <label htmlFor="reg-uname">UserName</label>
+        <input
+          id="reg-uname"
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+        />
+        <input type="submit" value="Register" />
+      </form>
+    </div>
+  );
 }
