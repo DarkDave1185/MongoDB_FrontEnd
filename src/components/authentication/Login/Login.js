8130b751bdb1b58b258befe7007163f0d5f3b0a5
DarkDave <darkdave1185@gmail.com>
2020-12-02 11:50:03 -0700

    full auth complete need styling

--- src/components/authentication/Login/Login.js
+++ src/components/authentication/Login/Login.js
@@ -1,5 +1,58 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import Axios from "axios";
+import { useHistory } from "react-router-dom";
+import UserContext from "../../../context/UserContext";
+import ErrorNotice from "../../misc/ErrorNotice";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState();
+
+  const { setUserData } = useContext(UserContext);
+  let history = useHistory();
+
+  const submit = async (e) => {
+    e.preventDefault();
+    try {
+      const loginUser = { email, password };
+      const loginResponse = await Axios.post(
+        "http://localhost:5000/users/login",
+        loginUser
+      );
+      setUserData({
+        token: loginResponse.data.token,
+        user: loginResponse.data.user,
+      });
+      localStorage.setItem("auth-token", loginResponse.data.token);
+      history.push("/");
+    } catch (err) {
+      err.response.data.msg && setError(err.response.data.msg);
+    }
+  };
+
+  return (
+    <div className="login">
+      <h3>Login</h3>
+      {error && (
+        <ErrorNotice message={error} clearError={() => setError(undefined)} />
+      )}
+      <form onSubmit={submit} className="form">
+        <label htmlFor="log-email">Email</label>
+        <input
+          id="log-email"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="log-pass">Password</label>
+        <input
+          id="log-pass"
+          type="password"
+          autoComplete="new-password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
 }
--- src/components/authentication/Register/Register.js
+++ src/components/authentication/Register/Register.js
@@ -3,43 +3,48 @@ import Axios from "axios";
 import { useHistory } from "react-router-dom";
 import UserContext from "../../../context/UserContext";
 import "./styleRegister.css";
+import ErrorNotice from "../../misc/ErrorNotice";
 
 export default function Register() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [passwordCheck, setPasswordCheck] = useState();
   const [displayName, setDisplayName] = useState();
-  /* const [error, setError] = useState(); */
+  const [error, setError] = useState();
 
   const { setUserData } = useContext(UserContext);
   let history = useHistory();
 
   const submit = async (e) => {
     e.preventDefault();
-    /* try { */
-    const newUser = { email, password, passwordCheck, displayName };
-    await Axios.post("http://localhost:5000/users/register", newUser);
-    const loginResponse = await Axios.post(
-      "http://localhost:5000/users/login",
-      {
-        email,
-        password,
-      }
-    );
-    setUserData({
-      token: loginResponse.data.token,
-      user: loginResponse.data.user,
-    });
-    localStorage.setItem("auth-token", loginResponse.data.token);
-    history.push("/");
-  }; /* catch (err) {
+
+    try {
+      const newUser = { email, password, passwordCheck, displayName };
+      await Axios.post("http://localhost:5000/users/register", newUser);
+      const loginResponse = await Axios.post(
+        "http://localhost:5000/users/login",
+        {
+          email,
+          password,
+        }
+      );
+      setUserData({
+        token: loginResponse.data.token,
+        user: loginResponse.data.user,
+      });
+      localStorage.setItem("auth-token", loginResponse.data.token);
+      history.push("/");
+    } catch (err) {
       err.response.data.msg && setError(err.response.data.msg);
     }
-  }; */
+  };
 
   return (
     <div className="registration">
       <h3>Register</h3>
+      {error && (
+        <ErrorNotice message={error} clearError={() => setError(undefined)} />
+      )}
       <form onSubmit={submit} className="form">
         <label htmlFor="reg-email">Email</label>
         <input
